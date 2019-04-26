414715d3d072785f61dbcd79d1b248dc281acbf7
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-26 11:29:11 +0100

    Added data to seed.rb

--- CRITICAL_ROLE/db/seed.rb
+++ CRITICAL_ROLE/db/seed.rb
@@ -4,28 +4,72 @@ require_relative('../models/dungeon_master.rb')
 
 
 
-dm1 = DungeonMaster.new(
-  "dm_name" = "Calum",
-  "campaign" = "Curse of Stradh",
-  "level_req" = "2"
-)
+dm1 = DungeonMaster.new({
+  "dm_name" => "Calum",
+  "campaign" => "Curse of Stradh",
+  "level_req" => "2"
+})
 # dm1.save()
 
-dm2 = DungeonMaster.new(
-  "dm_name" = "John",
-  "campaign" = "Tomb of Annihilation",
-  "level_req" = "7"
-)
+dm2 = DungeonMaster.new({
+  "dm_name" => "John",
+  "campaign" => "Tomb of Annihilation",
+  "level_req" => "7"
+})
 # dm2.save()
 
 
-character1 = Character.new(
-  "player_name" = "Liam",
-  "character_name" = "Rurich",
-  "race" = "human",
-  "character_class" = "cleric",
+character1 = Character.new({
+  "player_name" => "Liam",
+  "character_name" => "Rurich",
+  "race" => "Human",
+  "character_class" => "Cleric",
+  "level" => "7",
+  "dm_id" => dm2.id
+})
+# character1.save()
 
-)
+character2 = Character.new({
+  "player_name" => "Ross",
+  "character_name" => "Kirta",
+  "race" => "Gnome",
+  "character_class" => "Wizard",
+  "level" => "9",
+  "dm_id" => dm2.id
+})
+#character2.save()
+
+character3 = Character.new({
+  "player_name" => "Thomas",
+  "character_name" => "Kayla",
+  "race" => "Aasimar",
+  "character_class" => "Barbarian",
+  "level" => "5",
+  "dm_id" => dm1.id
+})
+#character3.save()
+
+character4 = Character.new({
+  "player_name" => "Harry",
+  "character_name" => "Heskan",
+  "race" => "Dragonborn",
+  "character_class" => "Monk",
+  "level" => "8",
+  "dm_id" => dm2.id
+})
+#character4.save()
+
+character5 = Character.new({
+  "player_name" => "Calum",
+  "character_name" => "Koopa",
+  "race" => "Tortle",
+  "character_class" => "Sorceror",
+  "level" => "3",
+  "dm_id" => dm1.id
+})
+#character5.save()
+
+
 
 
 
