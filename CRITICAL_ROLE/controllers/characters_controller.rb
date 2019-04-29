35db1e5be319572e6f3246928a980a81ef630713
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-29 09:50:52 +0100

    added edit, new and create erb files in characters

--- CRITICAL_ROLE/controllers/characters_controller.rb
+++ CRITICAL_ROLE/controllers/characters_controller.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require('pry')
 require_relative('../models/character.rb')
 require_relative('../models/dungeon_master.rb')
 also_reload('../models/*')
@@ -9,7 +10,35 @@ get '/characters' do
   erb(:"characters/index")
 end
 
+get '/characters/new' do
+  @dms = DungeonMaster.all()
+  erb(:"characters/new")
+end
+
 get '/characters/:id' do
   @character = Character.find( params['id'].to_i )
   erb(:"characters/show")
+end
+
+post '/characters' do
+  @character = Character.new( params )
+  @character.save()
+  erb(:"characters/create")
+end
+
+get '/characters/:id/edit' do
+  @dms = DungeonMaster.all()
+  @character = Character.find( params['id'].to_i )
+  erb(:"characters/edit")
+end
+
+post '/characters/:id' do
+  @character = Character.new( params )
+  @character.update()
+  erb(:"characters/show")
+end
+
+post '/characters/:id/delete' do
+  Character.destroy(params['id'])
+  redirect to('/characters')
 end
